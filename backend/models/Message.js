22861b7fd0b9aeed5736bd3d9d0d7dcc6941e608
17Anurag Mishra <anuragmishra0521@gmail.com>
2025-03-15 23:50:45 +0530

    Backend codes Update

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 
 const MessageSchema = new mongoose.Schema({
-  groupId: { type: mongoose.Schema.Types.ObjectId, ref: 'Group', required: true },
+  groupId: { type: String, required: true },
   senderId: { type: String, required: true },
   senderName: { type: String, required: true },
   content: { type: String, required: true },
   timestamp: { type: Date, default: Date.now },
 });
 
-export default mongoose.model('Message', MessageSchema);
+export default mongoose.models.Message || mongoose.model('Message', MessageSchema);
