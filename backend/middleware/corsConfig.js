5e79b18e4a4661c1d5edc18b6a25dc823c7ec496
Anurag Mishra <115710545+17AnuragMishra@users.noreply.github.com>
2025-06-11 22:37:57 +0530

    Update corsConfig.js

--- backend/middleware/corsConfig.js
+++ backend/middleware/corsConfig.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3000',
+    'https://riderconnect.in',
+    'https://www.riderconnect.in',
     'https://riderconnect.vercel.app',
-    'http://192.168.1.51:3000',
     ...(process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : []),
   ];
   const corsOptions = {
